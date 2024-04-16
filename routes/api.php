82fba527921474499322e40c03ec69addb38f217
TskGunGun <stonebeach.dakar@gmail.com>
2024-04-16 17:49:46 +0900

    テストAPIのルーティグを追加

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get('/test', function(Request $request){
+    return "test ok";
+});
+
+Route::get('me', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
