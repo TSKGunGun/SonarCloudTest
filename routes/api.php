7def19a6038e967414d19076e630b76ef37c23a8
TskGunGun <stonebeach.dakar@gmail.com>
2024-04-17 07:15:28 +0900

    不要なルートの削除

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\testController;
 
-Route::get('/test', testController::class);
-
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('/me', function(Request $request){
         return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\Login;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::post('/login', Login::class);
